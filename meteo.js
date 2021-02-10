09f465478e6b2e4ee16e47747a4664eb2b57bdb6
Chemela <78839202+Chemela@users.noreply.github.com>
2021-02-10 23:28:00 +0100

    update

--- meteo.js
+++ meteo.js
@@ -1,10 +1,11 @@
 
 		function showCity(){
+			//alert(villeCourante); 
 			console.log(villeCourante); 
 			
 			// 1. Récupérer la valeur courante du menu 
 			// Pour ça on va faire :
-			let nomVilleSelectionnee = document.getElementById("vil-select").value;
+			let VilleSelect = document.getElementById("vil-select").value;
 			//var nomVilleSelectionnee = document.getElementById("vil-select").value;
 			
 			// 2. Récupérer la liste de toutes les div correspondant à une div de météo 
@@ -14,15 +15,22 @@
 			// 3. Parcourir la liste des div et cacher celles qui ne sont pas sélectionnées.
 			// TODO : definir l : la taille max de votre tableau cities
 			var l = cities.length;
-			for (i=0; i<l; i++) {
-				if (cities[i].id == nomVilleSelectionnee) {
+			for (i=0; i<l; i++) { 
+			
+				if (VilleSelect ==""){
+					cities[i].style.display= "";
+				}else{
+				
+				if (cities[i].id == VilleSelect) {
 				  // garder l'element visible -> ELEMENT.style.display= "none";
-				  id.style.display= "block";
+				  cities[i].style.display= "none";
 				} else {
 				  // cacher l'element -> ELEMENT.style.display= "";
 				  //cities.style.visibility= "hidden";
-				  id.style.display= "none";
+				  cities[i].style.display= "";
 				}
+				}
+			
 			}
 		}
 		let villeCourante = "Lyon";
